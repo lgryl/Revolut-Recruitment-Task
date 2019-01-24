5b78617c0042a821eec062f451daad8b92c4a245
Łukasz Gryl <lukasz.gryl@gmail.com>
2019-01-24 20:37:47 +0100

    Setup converter view controller's view

--- RevolutTestApplication/ConverterViewController.swift
+++ RevolutTestApplication/ConverterViewController.swift
@@ -10,4 +10,30 @@ import UIKit
 
 class ConverterViewController: UIViewController {
     
+    let tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: tableView.trailingAnchor),
+            view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: tableView.bottomAnchor)
+            ])
+    }
+    
 }
