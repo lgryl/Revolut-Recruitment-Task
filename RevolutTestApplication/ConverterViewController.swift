f66f628f5519c51db48d67802bb7755042052bd9
Łukasz Gryl <lukasz.gryl@gmail.com>
2019-01-24 20:32:22 +0100

    Create converter view controller. Setup AppDelegate's window. Set converter view controller as its root view controller

---
+++ RevolutTestApplication/ConverterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConverterViewController.swift
+//  RevolutTestApplication
+//
+//  Created by lg on 24/01/2019.
+//  Copyright © 2019 lg. All rights reserved.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController {
+    
+}
