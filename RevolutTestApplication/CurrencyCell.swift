74788c4125cba40047eb6044a5d6bcaefd149f92
Łukasz Gryl <lukasz.gryl@gmail.com>
2019-01-26 09:02:08 +0100

    Focus on the text field on cell selection

--- RevolutTestApplication/CurrencyCell.swift
+++ RevolutTestApplication/CurrencyCell.swift
@@ -27,4 +27,11 @@ class CurrencyCell: UITableViewCell {
         flagImageView.image = flagImage
     }
     
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if selected {
+            amountTextField.becomeFirstResponder()
+        }
+    }
+    
 }
