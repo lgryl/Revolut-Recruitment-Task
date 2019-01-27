7c7f0485d90639b4c672330c30f5dd1178209164
Łukasz Gryl <lukasz.gryl@gmail.com>
2019-01-27 13:38:05 +0100

    Refactor amount validator

--- RevolutTestApplication/Validators/AmountValidator.swift
+++ RevolutTestApplication/Validators/AmountValidator.swift
@@ -10,16 +10,15 @@ import Foundation
 
 class AmountValidator {
     
-    static let pattern = "^((0|([1-9]\\d{0,6}))(\(LocaleHelper.escapedDecimalSeparator)\\d{0,2})?)?$"
-    let regularExpression: NSRegularExpression?
-    
-    init() {
-        regularExpression = try? NSRegularExpression(pattern: AmountValidator.pattern, options: [])
+    static var regularExpression: NSRegularExpression? {
+        let pattern = "^((0|([1-9]\\d{0,6}))(\(LocaleHelper.escapedDecimalSeparator)\\d{0,2})?)?$"
+        let regularExpression = try? NSRegularExpression(pattern: pattern, options: [])
+        return regularExpression
     }
     
     func isValid(_ amount: String) -> Bool {
         let range = NSRange(location: 0, length: amount.count)
-        let fullMatch = regularExpression?.numberOfMatches(in: amount, options: [.anchored], range: range) == 1
+        let fullMatch = AmountValidator.regularExpression?.numberOfMatches(in: amount, options: [.anchored], range: range) == 1
         return fullMatch
     }
     
