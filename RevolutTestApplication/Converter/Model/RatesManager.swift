14ce3dfd294675fd38bd8311a8a09764270713e5
Łukasz Gryl <lukasz.gryl@gmail.com>
2019-01-27 17:31:30 +0100

    Remove fake model. Do not display anything but the base currency at the start. Display other currencies only after the fetch.

--- RevolutTestApplication/Converter/Model/RatesManager.swift
+++ RevolutTestApplication/Converter/Model/RatesManager.swift
@@ -12,40 +12,7 @@ class RatesManager {
     
     static let baseCurrencyCode = "EUR"
     
-    var rates: [String: Decimal] = [
-        "AUD": 1.6111,
-        "BGN": 1.9494,
-        "BRL": 4.776,
-        "CAD": 1.5288,
-        "CHF": 1.1238,
-        "CNY": 7.919,
-        "CZK": 25.63,
-        "DKK": 7.4322,
-        "GBP": 0.89528,
-        "HKD": 9.1024,
-        "HRK": 7.4096,
-        "HUF": 325.42,
-        "IDR": 17267.0,
-        "ILS": 4.1569,
-        "INR": 83.442,
-        "ISK": 127.38,
-        "JPY": 129.12,
-        "KRW": 1300.5,
-        "MXN": 22.292,
-        "MYR": 4.7962,
-        "NOK": 9.7438,
-        "NZD": 1.7575,
-        "PHP": 62.386,
-        "PLN": 4.3041,
-        "RON": 4.6232,
-        "RUB": 79.313,
-        "SEK": 10.556,
-        "SGD": 1.5947,
-        "THB": 38.005,
-        "TRY": 7.6031,
-        "USD": 1.1596,
-        "ZAR": 17.765
-    ]
+    var rates: [String: Decimal] = [:]
     
     public func rate(for currencyCode: String) -> Decimal {
         guard currencyCode != RatesManager.baseCurrencyCode else {
